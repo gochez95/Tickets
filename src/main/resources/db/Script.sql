658ca25cb978ed7c9842daca3713a23d6d844603
gochez95 <ngochez95@gmail.com>
2022-06-10 10:48:49 -0500

    edit script sql

--- src/main/resources/db/Script.sql
+++ src/main/resources/db/Script.sql
@@ -1,219 +1,275 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `tickets` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tickets`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: tickets
+-- ------------------------------------------------------
+-- Server version	8.0.25
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema tickets
--- -----------------------------------------------------
+--
+-- Table structure for table `category`
+--
 
--- -----------------------------------------------------
--- Schema tickets
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `tickets` DEFAULT CHARACTER SET utf8 ;
-USE `tickets` ;
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id_category` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(75) NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tickets`.`rol`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`rol` (
-  `id_rol` INT NOT NULL AUTO_INCREMENT,
-  `name_rol` VARCHAR(75) NOT NULL,
-  `active` TINYINT(1) NULL,
-  PRIMARY KEY (`id_rol`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `category`
+--
 
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'category1'),(2,'category2'),(3,'category3'),(4,'category4');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `tickets`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`user` (
-  `id_user` INT NOT NULL AUTO_INCREMENT,
-  `name_user` VARCHAR(150) NOT NULL,
-  `lastname_user` VARCHAR(150) NOT NULL,
-  `user_name` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(150) NOT NULL,
-  `email_user` VARCHAR(150) NOT NULL,
-  `id_role` INT NULL,
-  `active` TINYINT(1) NULL,
-  PRIMARY KEY (`id_user`),
-  INDEX `id_rol_fk_idx` (`id_role` ASC) VISIBLE,
-  CONSTRAINT `id_rol_fk`
-    FOREIGN KEY (`id_role`)
-    REFERENCES `tickets`.`rol` (`id_rol`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Table structure for table `priority`
+--
 
+DROP TABLE IF EXISTS `priority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `priority` (
+  `id_priority` int NOT NULL AUTO_INCREMENT,
+  `name_priority` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (`id_priority`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tickets`.`category`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`category` (
-  `id_category` INT NOT NULL AUTO_INCREMENT,
-  `category_name` VARCHAR(75) NOT NULL,
-  PRIMARY KEY (`id_category`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `priority`
+--
 
+LOCK TABLES `priority` WRITE;
+/*!40000 ALTER TABLE `priority` DISABLE KEYS */;
+INSERT INTO `priority` VALUES (1,'Priority1'),(2,'Priority2'),(3,'Priority3'),(4,'Priority4');
+/*!40000 ALTER TABLE `priority` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `tickets`.`priority`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`priority` (
-  `id_priority` INT NOT NULL AUTO_INCREMENT,
-  `name_priority` VARCHAR(75) NULL,
-  PRIMARY KEY (`id_priority`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `rol`
+--
 
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol` (
+  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `name_rol` varchar(75) NOT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tickets`.`ticket`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`ticket` (
-  `id_ticket` INT NOT NULL,
-  `name_ticket` VARCHAR(150) NOT NULL,
-  `description_ticket` VARCHAR(350) NOT NULL,
-  `create_at` DATE NULL,
-  `finish_at` DATE NULL,
-  `id_category` INT NULL,
-  `id_priority` INT NULL,
-  `id_user` INT NULL,
-  `assing` INT NULL,
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (1,'admin',1),(2,'seller',1),(3,'user',1),(4,'supervisor',1);
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status` (
+  `id_status` int NOT NULL AUTO_INCREMENT,
+  `status_name` varchar(75) NOT NULL,
+  PRIMARY KEY (`id_status`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Create'),(2,'Escalated'),(3,'Closed');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket` (
+  `id_ticket` int NOT NULL,
+  `name_ticket` varchar(150) NOT NULL,
+  `description_ticket` varchar(350) NOT NULL,
+  `create_at` date DEFAULT NULL,
+  `finish_at` date DEFAULT NULL,
+  `id_category` int DEFAULT NULL,
+  `id_priority` int DEFAULT NULL,
+  `id_user` int DEFAULT NULL,
+  `assign` int DEFAULT NULL,
   PRIMARY KEY (`id_ticket`),
-  INDEX `id_category_fk_idx` (`id_category` ASC) VISIBLE,
-  INDEX `id_priority_fk_idx` (`id_priority` ASC) VISIBLE,
-  INDEX `id_user_idx` (`id_user` ASC) VISIBLE,
-  INDEX `user_assing_fk_idx` (`assing` ASC) VISIBLE,
-  CONSTRAINT `id_category_fk`
-    FOREIGN KEY (`id_category`)
-    REFERENCES `tickets`.`category` (`id_category`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `id_priority_fk`
-    FOREIGN KEY (`id_priority`)
-    REFERENCES `tickets`.`priority` (`id_priority`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `id_user`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `tickets`.`user` (`id_user`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `user_assing_fk`
-    FOREIGN KEY (`assing`)
-    REFERENCES `tickets`.`user` (`id_user`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `id_category_fk_idx` (`id_category`),
+  KEY `id_priority_fk_idx` (`id_priority`),
+  KEY `id_user_idx` (`id_user`),
+  KEY `assign_user_idx` (`assign`),
+  CONSTRAINT `assign_user` FOREIGN KEY (`assign`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_category_fk` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_priority_fk` FOREIGN KEY (`id_priority`) REFERENCES `priority` (`id_priority`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `ticket`
+--
 
--- -----------------------------------------------------
--- Table `tickets`.`status`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`status` (
-  `id_status` INT NOT NULL AUTO_INCREMENT,
-  `status_name` VARCHAR(75) NOT NULL,
-  PRIMARY KEY (`id_status`))
-ENGINE = InnoDB;
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+INSERT INTO `ticket` VALUES (1,'ticket1','ticket100000asignado','2022-03-05',NULL,1,1,3,6),(2,'ticket2','ticket2','2022-06-01',NULL,2,1,3,4),(3,'ticket2','ticket2','2022-06-13',NULL,3,3,3,6);
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `ticket_comment`
+--
 
--- -----------------------------------------------------
--- Table `tickets`.`ticket_status`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`ticket_status` (
-  `id_ticket` INT NOT NULL,
-  `id_status` INT NOT NULL,
-  PRIMARY KEY (`id_ticket`, `id_status`),
-  INDEX `id_status_fk_idx` (`id_status` ASC) VISIBLE,
-  CONSTRAINT `id_ticket_fk`
-    FOREIGN KEY (`id_ticket`)
-    REFERENCES `tickets`.`ticket` (`id_ticket`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `id_status_fk`
-    FOREIGN KEY (`id_status`)
-    REFERENCES `tickets`.`status` (`id_status`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `tickets`.`ticket_comment`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`ticket_comment` (
-  `id_ticket_comment` INT NOT NULL,
-  `comment` VARCHAR(350) NULL,
-  `id_ticket` INT NULL,
+DROP TABLE IF EXISTS `ticket_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket_comment` (
+  `id_ticket_comment` int NOT NULL,
+  `comment` varchar(350) DEFAULT NULL,
+  `id_ticket` int DEFAULT NULL,
   PRIMARY KEY (`id_ticket_comment`),
-  INDEX `id_ticket_comment_fk_idx` (`id_ticket` ASC) VISIBLE,
-  CONSTRAINT `id_ticket_comment_fk`
-    FOREIGN KEY (`id_ticket`)
-    REFERENCES `tickets`.`ticket` (`id_ticket`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+  KEY `id_ticket_comment_fk_idx` (`id_ticket`),
+  CONSTRAINT `id_ticket_comment_fk` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id_ticket`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `ticket_comment`
+--
 
--- -----------------------------------------------------
--- Table `tickets`.`user_tickets`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tickets`.`user_tickets` (
-  `id_ticket` INT NOT NULL,
-  `id_user` INT NOT NULL,
-  PRIMARY KEY (`id_ticket`, `id_user`),
-  INDEX `id_user_fk__idx` (`id_user` ASC) VISIBLE,
-  CONSTRAINT `id_ticket_fk_`
-    FOREIGN KEY (`id_ticket`)
-    REFERENCES `tickets`.`ticket` (`id_ticket`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `id_user_fk_`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `tickets`.`user` (`id_user`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+LOCK TABLES `ticket_comment` WRITE;
+/*!40000 ALTER TABLE `ticket_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket_comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE `tickets`.`ticket` 
-ADD COLUMN `assign` INT NULL AFTER `id_user`,
-ADD INDEX `assign_user_idx` (`assign` ASC) VISIBLE;
-;
-ALTER TABLE `tickets`.`ticket` 
-ADD COLUMN `assign` INT NULL AFTER `id_user`,
-ADD INDEX `assign_user_idx` (`assign` ASC) VISIBLE;
-;
-ALTER TABLE `tickets`.`ticket` 
-ADD CONSTRAINT `assign_user`
-  FOREIGN KEY (`assign`)
-  REFERENCES `tickets`.`user` (`id_user`)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE;
+--
+-- Table structure for table `ticket_status`
+--
 
-CREATE USER 'user1';
+DROP TABLE IF EXISTS `ticket_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket_status` (
+  `id_ticket` int NOT NULL,
+  `id_status` int NOT NULL,
+  PRIMARY KEY (`id_ticket`,`id_status`),
+  KEY `id_status_fk_idx` (`id_status`),
+  CONSTRAINT `id_status_fk` FOREIGN KEY (`id_status`) REFERENCES `status` (`id_status`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_ticket_fk` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id_ticket`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `tickets`.`rol` (`name_rol`, `active`) VALUES ('admin', '1');
-INSERT INTO `tickets`.`rol` (`name_rol`, `active`) VALUES ('seller', '1');
-INSERT INTO `tickets`.`rol` (`name_rol`, `active`) VALUES ('user', '1');
-INSERT INTO `tickets`.`rol` (`name_rol`, `active`) VALUES ('supervisor', '1');
+--
+-- Dumping data for table `ticket_status`
+--
 
-INSERT INTO `tickets`.`user` (`name_user`, `lastname_user`, `user_name`, `password`, `email_user`, `id_role`, `active`) VALUES ('Telus', 'Telus', 'admin', '$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K', 'telus@gmail.com', '1', '1');
-INSERT INTO `tickets`.`user` (`name_user`, `lastname_user`, `user_name`, `password`, `email_user`, `id_role`, `active`) VALUES ('Juan', 'Perez', 'perez', '$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K', 'p@gmail.com', '2', '1');
-INSERT INTO `tickets`.`user` (`name_user`, `lastname_user`, `user_name`, `password`, `email_user`, `id_role`, `active`) VALUES ('Luz', 'Maria', 'luz', '$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K', 'luz@gmail.com', '3', '1');
-INSERT INTO `tickets`.`user` (`name_user`, `lastname_user`, `user_name`, `password`, `email_user`, `id_role`, `active`) VALUES ('Jose', 'Lopez', 'lopez1', '$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K', 'lopez@gmail.com', '4', '1');
+LOCK TABLES `ticket_status` WRITE;
+/*!40000 ALTER TABLE `ticket_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket_status` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `user`
+--
 
-INSERT INTO `tickets`.`category` (`category_name`) VALUES ('category1');
-INSERT INTO `tickets`.`category` (`category_name`) VALUES ('category2');
-INSERT INTO `tickets`.`category` (`category_name`) VALUES ('category3');
-INSERT INTO `tickets`.`category` (`category_name`) VALUES ('category4');
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `name_user` varchar(150) NOT NULL,
+  `lastname_user` varchar(150) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `email_user` varchar(150) NOT NULL,
+  `id_role` int DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  KEY `id_rol_fk_idx` (`id_role`),
+  CONSTRAINT `id_rol_fk` FOREIGN KEY (`id_role`) REFERENCES `rol` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `user`
+--
 
-INSERT INTO `tickets`.`status` (`status_name`) VALUES ('Create');
-INSERT INTO `tickets`.`status` (`status_name`) VALUES ('Escalated');
-INSERT INTO `tickets`.`status` (`status_name`) VALUES ('Closed');
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Telus','Telus','admin','$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K','ngochez95@gmail.com',1,1),(2,'Elena','Figueroa','Figueroa','$2a$10$wu.THDpBoKs39DdceQhmceE4kXiBKk3B3yxqtELWibjZCXDv8lDkK','figueroa@gmail.com',3,1),(3,'Jose','Perez','josesv','$2a$10$GdfSB7XHgMD7qS/Obn23Z.LlxK4t6iN316mwGid7jQu32voCFtNR.','joseperez@gmail.com',3,1),(4,'chepe','aviles','chepe','$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K','chepe@gmail.com',2,1),(5,'Jose','Lopez','lopez1','$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K','lopez@gmail.com',4,1),(6,'Maria','Aguilar','aguilar','$2a$10$3VqCdIiiEBa4Nh47seGIXuWCd5GeDYZ.eY/QZwsueUuN0m2hi4c2K','aguilar@gmail.com',2,1),(7,'nelson','gochez','ngochez95','$2a$10$aWwLGim3NN4fSq/E.Zmen.fpVnZxpXwkWgF5tpwtWi1NkdkRHmw9K','ngochez95@gmail.com',3,1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Table structure for table `user_tickets`
+--
+
+DROP TABLE IF EXISTS `user_tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_tickets` (
+  `id_ticket` int NOT NULL,
+  `id_user` int NOT NULL,
+  PRIMARY KEY (`id_ticket`,`id_user`),
+  KEY `id_user_fk__idx` (`id_user`),
+  CONSTRAINT `id_ticket_fk_` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id_ticket`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_user_fk_` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_tickets`
+--
+
+LOCK TABLES `user_tickets` WRITE;
+/*!40000 ALTER TABLE `user_tickets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_tickets` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-10 10:20:56
